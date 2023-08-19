1b7e7db9c7aef9f8bc328c144bf3a96301aac646
Prodject <Liter.Hari@gmail.com>
2023-08-19 15:35:16 +0200

    first commit

---
+++ Counter/Counter.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <Windows.h>
+
+class Counter {
+public:
+	int count=1;
+	int countAdd() {
+		count++;
+		return count;
+	}
+	int countSubtract() {
+		count--;
+		return count;
+	}	
+};
+
+int main() {
+	Counter count;
+	std::string question;	
+	char keyCode;
+
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+	std::cin >> question;
+	if (question == "да")
+	{
+	 std::cout << "Введите начальное значение счётчика: ";
+	 std::cin  >> count.count;			
+	}else {
+		std::cout << "Начальное значение счётчика: " << count.count << std::endl;
+	}
+
+	while (true)
+	{		
+		std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+		std::cin >> keyCode;
+
+		if (keyCode == '+') {
+			count.countAdd();
+			continue;
+		}
+
+		if (keyCode == '-') {
+			count.countSubtract();
+			continue;
+		}
+
+		if (keyCode == '=') {
+			std::cout << count.count << std::endl;
+			continue;
+		}		
+
+		if (keyCode == 'х') {
+			std::cout << "До свидания!" << std::endl;
+			return 0;
+		}
+	}	
+}
